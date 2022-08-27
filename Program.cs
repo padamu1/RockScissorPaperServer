54e372314cb27d70a03ccf8d6177b94dda90ea97
mywatt <padamu1@naver.com>
2022-08-27 23:32:47 +0900

    초기 커밋, 웹 소켓 및 connection manager 추가 - 인터넷 정보 참고

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+using SimulFactory.Core;
+public class Program
+{
+    public static void Main()
+    {
+        ConnectionManager cm = new ConnectionManager("127.0.0.1",80);
+        bool isServerStop = false;
+        while (!isServerStop)
+        {
+            Console.Write("\n\n\n\nServer For RockScissorPaper\n");
+            Console.WriteLine("서버를 종료하시려면 \"quit\"를 입력해주세요.");
+            switch (Console.ReadLine())
+            {
+                case "quit":
+                    isServerStop = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Script/Core/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SimulFactory.Core
+{
+    public class ConnectionManager
+    {
+        private readonly TcpListener tcpListener;
+
+        public ConnectionManager(string address, int port)
+        {
+            tcpListener = new TcpListener(IPAddress.Parse(address), port);
+            tcpListener.Start();
+            //비동기 Listening 시작
+            tcpListener.BeginAcceptTcpClient(OnAcceptClient, null);
+        }
+
+        private void OnAcceptClient(IAsyncResult ar)
+        {
+            TcpClient client = tcpListener.EndAcceptTcpClient(ar);
+            Console.WriteLine("Client Connected");
+            WebSocketController webSocketController = new WebSocketController(client);
+            //다음 클라이언트를 대기
+            tcpListener.BeginAcceptTcpClient(OnAcceptClient, null);
+        }
+    }
+}
