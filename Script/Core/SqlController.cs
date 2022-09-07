0343f2e5cebb000023728b62f1d02f3b3a943d6d
김선오 <padamu1@naver.com>
2022-09-07 23:20:29 +0900

    sql 에셋 임포트

---
+++ Script/Core/SqlController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+
+namespace SimulFactory.Core
+{
+    /// <summary>
+    /// 사전에 Sql 과 연결을 유지하고 확인하는 작업 후 util 에서 끌어다 작업함.
+    /// </summary>
+    public class SqlController
+    {
+        static readonly Lazy<SqlController> instanceHolder = new Lazy<SqlController>(() => new SqlController());
+        public static SqlController GetInstance()
+        {
+            return instanceHolder.Value;
+        }
+        private SqlConnection mssqlconn;
+        /// <summary>
+        /// SqlController 생성자
+        /// </summary>
+        public SqlController()
+        {
+            string strConn = "Data Source=192.168.0.1,1433:Initial Catalog=DataBase;User Id =user1;Password=1234";
+            mssqlconn = new SqlConnection(strConn);
+            mssqlconn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = mssqlconn;
+        }
+        /// <summary>
+        /// SqlConnection 반환
+        /// </summary>
+        /// <returns></returns>
+        public SqlConnection GetSqlConnection()
+        {
+            return mssqlconn;
+        }
+    }
+}
--- Script/Core/Util/SqlUtil.cs
+++ Script/Core/Util/SqlUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace SimulFactory.Core.Util
     {
         public static bool UpdateSql()
         {
+            SqlConnection connection = SqlController.GetInstance().GetSqlConnection();
+            connection.Open();
+
             return false;
         }
     }
