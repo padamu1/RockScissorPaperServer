119bf20dab687d207acdb1d760243cd76d3f7e4b
mywatt <padamu1@naver.com>
2022-09-12 23:54:50 +0900

    쓰레드 시스템 추가

---
+++ Script/Core/Base/ThreadBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulFactory.Core.Base
+{
+    public class ThreadBase : IDisposable
+    {
+        protected int delayTime;
+        protected bool isThreadStop;
+        protected Thread thread;
+
+        /// <summary>
+        /// 스레드 생성자
+        /// </summary>
+        /// <param name="delayTime">딜레이 타임</param>
+        public ThreadBase()
+        {
+            isThreadStop = false;
+            thread = new Thread(ThreadAction);
+        }
+        /// <summary>
+        /// 스레드 시작
+        /// </summary>
+        public void ThreadStart(int delayTime)
+        {
+            this.delayTime = delayTime;
+            thread.Start();
+        }
+        /// <summary>
+        /// 스레드 실제 동작 부분
+        /// </summary>
+        protected virtual void ThreadAction()
+        {
+            // 상속받은 클래스에서 재정의 필요
+        }
+        /// <summary>
+        /// 스레드 삭제
+        /// </summary>
+        public void Dispose()
+        {
+            isThreadStop = true;
+            Console.WriteLine(GetThreadName() + " thread stopped");
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// 스레드 복사에 사용
+        /// </summary>
+        /// <returns>상속받은 객체</returns>
+        public virtual ThreadBase Clone()
+        {
+            // 상속받은 클래스에서 재정의 필요
+            return this;
+        }
+        /// <summary>
+        /// 스레드 이름 정의 - 클래스와 같게 설정
+        /// </summary>
+        /// <returns>스레드 이름</returns>
+        public virtual string GetThreadName()
+        {
+            return "ThreadBase";
+        }
+    }
+}
---
+++ Script/Core/ThreadManager.cs
@@ -0,0 +1,100 @@
+﻿using SimulFactory.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulFactory.Core
+{
+    /// <summary>
+    /// 스레드 테스트용 클래스
+    /// </summary>
+    public class Worker : ThreadBase
+    {
+        private int workCount=0;
+        public Worker():base()
+        {
+            // 생성자
+        }
+        protected override void ThreadAction()
+        {
+            while (!isThreadStop)
+            {
+                Work();
+                Console.Write(thread.Name);
+                Thread.Sleep(delayTime);
+            }
+        }
+        public void Work()
+        {
+            Console.WriteLine("Working..." + workCount++);
+        }
+        public override ThreadBase Clone()
+        {
+            Worker worker = new Worker();
+            worker.workCount = workCount;
+            worker.delayTime = delayTime;
+            return worker;
+        }
+        public override string GetThreadName()
+        {
+            return "Worker";
+        }
+
+    }
+    public class ThreadManager
+    {
+        static readonly Lazy<ThreadManager> instanceHolder = new Lazy<ThreadManager>(() => new ThreadManager());
+        public static ThreadManager GetInstance()
+        {
+            return instanceHolder.Value;
+        }
+
+        private Dictionary<string, ThreadBase> threadPoolDic; // 다양한 스레드를 담기 위한 풀 딕셔너리
+        public ThreadManager()
+        {
+            threadPoolDic = new Dictionary<string, ThreadBase>();
+        }
+        /// <summary>
+        /// 사용할 스레드를 풀에 등록
+        /// </summary>
+        /// <param name="threadName"></param>
+        /// <param name="thread"></param>
+        public void RegistThread(string threadName,ThreadBase thread)
+        {
+            if(!threadPoolDic.ContainsKey(threadName))
+            {
+                threadPoolDic.Add(threadName, thread);
+                Console.WriteLine(threadName + " registered successfully");
+            }
+            else
+            {
+                Console.WriteLine(threadName + " already registered");
+            }
+        }
+        /// <summary>
+        /// 스레드 풀안에 있는 객체를 가져와 딜레이 타임을 적용해 동작하는 스레드를 만드는 함수
+        /// </summary>
+        /// <param name="threadName"></param>
+        /// <param name="delayTime"></param>
+        public void MakeThread(string threadName,int delayTime)
+        {
+            if(threadPoolDic.ContainsKey(threadName))
+            {
+                ThreadBase tb = threadPoolDic[threadName].Clone();
+                tb.ThreadStart(delayTime);
+                Console.WriteLine(threadName + " thread start");
+            }
+        }
+        /// <summary>
+        /// 등록된 스레드를 삭제하는 함수
+        /// </summary>
+        /// <param name="thread"></param>
+        public void RemoveWorker(ThreadBase tb)
+        {
+            Console.WriteLine(tb.GetThreadName() + " thread will stop");
+            tb.Dispose();
+        }
+    }
+}
