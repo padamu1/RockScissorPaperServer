a53b1149827ff11670e7172a2526297612fc462c
김선오 <padamu1@naver.com>
2022-10-16 14:30:12 +0900

    서버 관리 별도 쓰레드에서 처리

--- Program.cs
+++ Program.cs
@@ -3,14 +3,48 @@ using System.Net;
 using System.Net.Sockets;
 
 using SimulFactory.Core;
+using SimulFactory.Core.Base;
 using SimulFactory.Game;
 
+public class ApplicationManager : ThreadBase
+{
+    protected override void ThreadAction()
+    {
+        base.ThreadAction();
+
+        bool isServerStop = false;
+        while (!isServerStop)
+        {
+            Console.Write("\nServer For ROCK_SCISSOR_PAPER\n");
+            Console.WriteLine("서버를 종료하시려면 \"quit\"를 입력해주세요.");
+            switch (Console.ReadLine())
+            {
+                case "Stop":
+                    Console.WriteLine("서비스가 종료됩니다.");
+                    NetworkStarter.GetInstance().ServiceStop();
+                    break;
+                case "Start":
+                    Console.WriteLine("서비스가 시작됩니다.");
+                    NetworkStarter.GetInstance().ServiceStart();
+                    break;
+                case "Quit":
+                    Console.WriteLine("프로그램이 종료됩니다.");
+                    isServerStop = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
 public class Program
 {
     public static void Main()
     {
         NetworkStarter.GetInstance();
-        while(true)
+        ApplicationManager applicationManager = new ApplicationManager();
+        applicationManager.ThreadStart(1);
+        while (true)
         {
 
         }
