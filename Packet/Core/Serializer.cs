99fe93c83375114424b2ccc541013d06acd58d65
mywatt <padamu1@naver.com>
2023-01-15 01:00:14 +0900

    Serializer 파일 추가

---
+++ Packet/Core/Serializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockScissorPaperServer.Packet.Core
+{
+    public class Serializer
+    {
+        public byte[] GetBytes(int type, object value)
+        {
+            switch(type)
+            {
+                case 0:
+                    return LongToBytes((long)value);
+                default:
+                    return new byte[0];
+            }
+        }
+
+        public byte[] LongToBytes(long value) => BitConverter.GetBytes(value);
+    }
+}
