1f5d7e833e1b2e506a7c1354d870a5a26205aa8b
김선오 <padamu1@naver.com>
2023-02-02 22:26:54 +0900

    유저 정보 내려주는 코드 수정

--- Script/Game/Event/S_UserInfo.cs
+++ Script/Game/Event/S_UserInfo.cs
@@ -1,4 +1,5 @@
 ﻿using RockScissorPaperServer.PacketSerializer.Model;
+using RockScissorPaperServer.Script.Common.Dto;
 using SimulFactory.Common.Bean;
 using SimulFactory.Common.Instance;
 using SimulFactory.Core.Util;
@@ -15,10 +16,17 @@ namespace SimulFactory.Game.Event
         public static PacketData Data(PcInstance pc)
         {
             Dictionary<byte, object> param = new Dictionary<byte, object>();
+            // 유저 이름
             param.Add(0, pc.GetUserData().UserName);
-            param.Add(1, pc.GetPcPvp().GetRating());
-            param.Add(2, pc.GetPcPvp().GetWinCount());
-            param.Add(3, pc.GetPcPvp().GetDefeatCount());
+            // 노말 매치
+            NormalPvpDto normalPvpDto = pc.GetPcPvp().GetNormalPvpDto();
+            param.Add(1, normalPvpDto.Rating);
+            param.Add(2, normalPvpDto.WinCount);
+            param.Add(3, normalPvpDto.DefeatCount);
+            // 멀티 매치
+            MultiPvpDto multiPvpDto = pc.GetPcPvp().GetMultiPvpDto();
+            param.Add(4, multiPvpDto.WinCount);
+            param.Add(5, multiPvpDto.DefeatCount);
             return new PacketData((byte)Define.EVENT_CODE.UserInfoS, param);
         }
     }
