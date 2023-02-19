c0f945ca492beeb552ea60315dfa87f5ffaecc00
김선오 <padamu1@naver.com>
2023-02-19 21:44:34 +0900

    코드 정리

--- Program.cs
+++ Program.cs
@@ -35,33 +35,9 @@ public class Program
 {
     public static void Main()
     {
-        
         NetworkStarter.GetInstance();
+        Console.WriteLine("서비스가 시작됩니다.");
+        NetworkStarter.GetInstance().ServiceStart();
         ApplicationManager.Start();
-        /*
-        bool isServerStop = false;
-        while (!isServerStop)
-        {
-            Console.Write("\nServer For ROCK_SCISSOR_PAPER\n");
-            Console.WriteLine("서버를 종료하시려면 \"quit\"를 입력해주세요.");
-            switch (Console.ReadLine())
-            {
-                case "Stop":
-                    Console.WriteLine("서비스가 종료됩니다.");
-                    NetworkStarter.GetInstance().ServiceStop();
-                    break;
-                case "Start":
-                    Console.WriteLine("서비스가 시작됩니다.");
-                    NetworkStarter.GetInstance().ServiceStart();
-                    break;
-                case "Quit":
-                    Console.WriteLine("프로그램이 종료됩니다.");
-                    isServerStop = true;
-                    break;
-                default:
-                    break;
-            }
-        }
-        */
     }
 }
